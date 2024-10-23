b62fb9e003b3866d5f95e0be36ed61fdb5b7bd4a
juanmediavillaa <jmediavillasaez@gmail.com>
2024-10-23 12:29:34 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
-import Index from './components/Index';
-import About from './components/About';
+// import Index from './components/Index';
+// import About from './components/About';
 import Services from './components/Services';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
 import Footer from './components/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
+//import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
 
-const router = createBrowserRouter(createRoutesFromElements(
+/* const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Services/>} />,
 ));
+*/
 
 function App() {
   return (
